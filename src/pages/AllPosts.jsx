322dfaa5402ad534375d5798477f05cddd3763b8
Saifullah Khan <73275780+saifullah72437@users.noreply.github.com>
2024-03-08 06:15:50 -0800

    responsive AllPosts.jsx

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -19,9 +19,9 @@ function AllPosts() {
   return (
     <div className='w-full py-8'>
         <Container>
-            <div className='flex items-start flex-wrap'>
+            <div className='flex flex-col sm:flex-row items-start flex-wrap'>
                 {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
+                    <div key={post.$id} className='p-2 sm:w-1/4 w-full'>
                         <PostCard {...post} />
                     </div>
                 ))}
